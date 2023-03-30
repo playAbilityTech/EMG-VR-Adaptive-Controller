7f50350f1701723bac78c83e03002ac7d4a0d0f2
Valentin Squirelo <valentin+github@hackerloop.com>
2023-03-30 14:40:18 +0200

    Create one button vr controller.ino

---
+++ one button vr controller.ino
@@ -0,0 +1,35 @@
+#include <Servo.h> // Include the Servo library to control the motor
+
+#define thresholdInPin 15 // Define the input pin for the MyoWare sensor threshold value
+#define myowareInPin 12 // Define the input pin for the MyoWare sensor value
+
+Servo myservo; // Create a Servo object
+static const int servoPin = 27; // Define the pin for the servo motor
+
+int myowareValue = 0; // Variable to store the MyoWare sensor value
+int threshold = 2000; // Initial threshold value to compare with the MyoWare sensor value
+
+void setup() {
+  pinMode(myowareInPin, INPUT); // Set the MyoWare sensor pin as an input
+  pinMode(thresholdInPin, INPUT); // Set the threshold pin as an input
+  pinMode(servoPin, OUTPUT); // Set the servo pin as an output
+  myservo.attach(servoPin); // Attach the servo motor to the servo pin
+  Serial.begin(9600); // Start serial communication at 9600 baud rate
+}
+
+void loop() {
+  myowareValue = analogRead(myowareInPin); // Read the MyoWare sensor value
+  threshold = analogRead(thresholdInPin); // Read the threshold value
+  Serial.println(myowareValue); // Print the MyoWare sensor value to the serial monitor
+  Serial.println(threshold); // Print the threshold value to the serial monitor
+
+  // If the MyoWare sensor value is greater than the threshold value
+  if (myowareValue > threshold) {
+    myservo.write(80); // Move the servo motor to the 80-degree position
+    delay(200); // Wait for 200 milliseconds
+    Serial.println("press"); // Print "press" to the serial monitor
+  } else {
+    myservo.write(130); // Move the servo motor to the 130-degree position
+  }
+  delay(100); // Wait for 100 milliseconds before the next loop iteration
+}
